a46f7c83d0943c394b7735506c2ced2f8253a2c8
Pedro Luiz <105122745+pedroooluiz@users.noreply.github.com>
2022-06-08 16:04:44 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,43 +1,53 @@
-def Soma():
-  a = float(input('Digite o primeiro numero: '))
-  b = float(input('Digite o segundo numero: '))
-  operacao = a + b
-  print(operacao)  
-def Subtracao():
-  c = float(input('Digite o primeiro numero: '))
-  d = float(input('Digite o segundo numero: '))
-  operacao = c - d
-  print(operacao)
-def Divisao():
-  e = float(input('Digite o primeiro numero: '))
-  f = float(input('Digite o segundo numero: '))
-  operacao = e / f
-  print(operacao)
-def Multiplicacao():
-  g = float(input('Digite o primeiro numero: '))
-  h = float(input('Digite o segundo numero: '))
-  operacao = g * h
-  print(operacao)
+def CalculadoraBasica():
+  lista = ['+', '-', '/', '*', '//', '%', 'exp', '**']
+  continuar = 's'
   
-continuar = 's'
-
-while continuar == 's':
-  try:
-    Calculadora = str(input('Digite a operação direto. Exemplo + (soma) e aperte enter, / (divisão) *(multiplicação) - (subtração): ')).strip()
-  
-    if Calculadora == '+':
-      Soma()
-    elif Calculadora == '-':
-      Subtracao()
-    elif Calculadora == '/':
-      Divisao()
-    elif Calculadora == '*':
-      Multiplicacao()
-    else:
-      print('Digite uma das operações.')
-    continuar = str(input('Deseja continuar? (S/N) ')).strip().lower()
-  except:
-    print('Precisa digitar uma das operações')
-
-
+  while continuar == 's':
+    try:
+      print("""Digite uma das operações: 
+            + (soma), 
+            / (divisão), 
+            * (multiplicação), 
+            - (subtração), 
+            // (quociente), 
+            % (resto da divisão), 
+            **(raiz quadrada de a e b), 
+            exp(expoente a sobre b): """)
+      Calculadora = str(input('Escolha uma das operações: ')).strip()
+      if Calculadora in lista:
+        a = float(input('Digite o primeiro numero: '))
+        b = float(input('Digite o segundo numero: '))
+      if Calculadora == '+':
+        operacao = a + b
+        print(operacao)  
+      elif Calculadora == '-':
+        operacao = a - b
+        print(operacao) 
+      elif Calculadora == '/':
+        operacao = a / b
+        print(operacao) 
+      elif Calculadora == '*':
+        operacao = a * b
+        print(operacao) 
+      elif Calculadora == '//':
+        operacao = a // b
+        print(operacao)
+      elif Calculadora == '%':
+        operacao = a % b
+        print(operacao)
+      elif Calculadora == 'exp':
+        operacao = a ^ b
+        print(operacao)
+      elif Calculadora == '**':
+        operacao1 = pow(a)
+        operacao2 = pow(b)
+        print(operacao1)
+        print(operacao2)
+      else:
+        print('Digite uma das operações.')
+        continuar = str(input('Deseja continuar? (S/N)       ')).strip().lower()
+      continuar = str(input('Deseja continuar? (S/N): ')).strip().lower()
+    except:
+      continuar = str(input('Deseja continuar? (S/N): ')).strip().lower()
+CalculadoraBasica()
 print('Obrigado por utilizar o nosso sistema.')
